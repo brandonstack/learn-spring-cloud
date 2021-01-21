8abaeeb5a95c03967c74f7f202a71f80022d859c
brandonstack <lxxdev@gmail.com>
2021-01-21 09:05:42 +0800

    init

---
+++ eureka-client/src/main/java/com/github/brandonstack/sc/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,16 @@
+package com.github.brandonstack.sc.eurekaclient;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaClientApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
